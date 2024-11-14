07d4dd3691e5ba854fb6429d73db3b564cd253f2
Thypham2211 <baothy22112003@gmail.com>
2024-11-14 15:40:06 +0100

    Alle Bug gefixt. Aber wo ist meine mp4 Datei???????? what the helllllll

---
+++ HAforreal.py
@@ -0,0 +1,125 @@
+#Gruppe       Gruppennummer
+#Name, Vorname, Matrikelnummer
+#Name, Vorname, Matrikelnummer
+#Name, Vorname, Matrikelnummer
+#Name, Vorname, Matrikelnummer
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+#from matplotlib.cm import binary
+from scipy.signal import convolve2d
+from skimage.io import imread
+from skimage.color import rgb2gray
+from skimage.transform import resize
+
+#import matplotlib
+#matplotlib.rcParams['animation.ffmpeg_path'] = '/opt/homebrew/bin/ffmpeg'
+from matplotlib.animation import FFMpegWriter
+
+#Erstelle den FFMpegWriter
+writer = FFMpegWriter(fps = 10, metadata = dict(artist = 'Me'), bitrate = 1800)
+
+
+#Um Schleife zu vermeiden benutze ich diese Funktion, um die Startkonfiguration  in Abhängigkeit von k zuzuweisen.
+def initialize_grid(n, k):
+	#Zufällige Verteilung (k = 1)
+	random_grid = np.random.choice([0, 1], size = (n, n), p =[0.5, 0.5])
+
+#	Tub mit Störung (k = 2)
+	tub = np.array([[0, 1, 1, 0],
+					[1, 0, 0, 1],
+					[1, 0, 0, 1],
+					[0, 1, 1, 0]])
+	mid = n // 2
+	tub_grid = np.zeros((n, n))
+	tub_grid[mid - 2:mid + 2, mid - 2:mid + 2] = tub
+	tub_grid[mid, mid] = 1
+	#Füge eine Störung hinzu
+
+	#Acorn-Struktur (k = 3)
+	acorn = np.array([[0, 1, 0, 0, 0, 0, 0],
+			  [0, 0, 0, 1, 0, 0, 0],
+			  [1, 1, 0, 0, 1, 1, 1]])
+	acorn_grid = np.zeros((n, n))
+	acorn_mid_row = mid - acorn.shape[0] // 2
+	acorn_mid_col = mid - acorn.shape[1] // 2
+	acorn_grid[acorn_mid_row:acorn_mid_row + acorn.shape[0],
+	acorn_mid_col:acorn_mid_col + acorn.shape[1]] = acorn
+
+#Bild einlesen (k = 4)
+	img = imread('ihr_Foto.jpg')
+	img_gray = rgb2gray(img)
+	#rgb2gray konvertiert das Bild in Graustufen (Helligkeit zwischen 0 und 1).
+	img_bin = img_gray > 0.5
+	#erstellt ein binäres Bild, wobei Pixel mit Helligkeit über 0,5 als "1" (schwarz) und darunter als "0"
+	#(weiß) gesetzt werden.
+	image_grid = resize(img_bin, (n, n)).astype(int)
+	#resize, damit das Bild auch n x n groß ist.
+
+	#Liste der Startkonfigurationen
+	configurations =[random_grid, tub_grid, acorn_grid, image_grid]
+
+	#Rückgabe der Startkonfiguration basierend auf k (0-basiert: k-1)
+	return configurations[k - 1]
+
+
+def NumSimHA2(z, n, J, k):
+	#Parameter:
+	#z = Startkonfiguration
+	z = initialize_grid(n, k)
+	#𝐽 Zeitschritten, k = vier Anfangsverteilungen und n = Gridgröße -> werden beim Ausführen definiert
+
+		#--------------------------------------------------
+	#Filter 'kernel', der bestimmt, wie die Nachbarschaft berechnet wird. Hier werden nur die ECHTE NACHBARN summiert.
+	kernel = np.array([[1, 1, 1],
+		   [1, 0, 1],
+		   [1, 1, 1]])
+
+	fig,ax = plt.subplots()
+	img = ax.imshow(z, cmap = 'binary')
+	#Liste zur Speicherung der Bilder für die Animation
+	frames =[]
+
+	#Wie funktioniert die Faltungconvolve2d?
+	#1.Der Filter wird auf das Gitter gelegt, sodass er mit einem 3x3-Bereich überlappt.
+	#2.Die Werte des Gitters und des Filters werden elementweise multipliziert.
+	#3.Die Ergebnisse werden addiert und ergeben einen neuen Wert im Ergebnisgitter 'Summe'.
+	#--------------------------------------------------
+
+	for t in range(J):
+		#Summe der Nachbarn (periodische Randbedingungen: wrap)
+		Summe = convolve2d(z, kernel, mode = 'same', boundary = 'wrap')
+
+		#Update-Regel:
+		#Bedingung erfüllt: True = 1, sonst: False = 0
+		z = (Summe == 3) | ((z == 1) & (Summe == 4))
+		z = z.astype(int)
+		#Konvertiere das Ergebnis zurück zu 0 und 1
+
+		#Update des Plots 'img.set_array(z)' und Hinzufügen zum Frames-Array
+		img.set_array(z)
+		frames.append([plt.imshow(z, cmap = 'binary', animated = True)])
+
+		t += 1
+
+		#Erstellung der Animation
+		#ArtistAnimation: Erstellt eine Animation aus der Liste frames.
+		#interval=100: Setzt die Zeit zwischen den Frames auf 100 Millisekunden.
+		#blit=True: Macht die Animation schneller, indem nur der Teil des Bildes aktualisiert wird, der sich ändert.
+		ani = animation.ArtistAnimation(fig, frames, interval = 100, blit = True)
+
+		#Speichern als MP4
+		#fps steht für "Frames per Second" (Bilder pro Sekunde).
+		ani.save("/NumSim_HA/ThyhihiNumSim/simulation_animation.mp4", writer = writer)
+		#writer="ffmpeg", fps=10)
+
+		#Anzeige der Animation im Jupyter Notebook
+		plt.colorbar()
+		plt.show()
+
+
+		#Beispiel für die Verwendung
+		n = 100
+		k = 2
+		J = 100
+		NumSimHA2(None, n, J, k)
